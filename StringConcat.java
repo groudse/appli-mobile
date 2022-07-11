673ebbcd04b813dc46d3777d7d08f6c824e465d6
Your Name <you@example.com>
2022-07-11 13:54:00 +0200

    tre

--- StringConcat.java
+++ StringConcat.java
@@ -1,5 +1,8 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if(s1!=null && s2!=null){
         return s1.concat(s2);
-    }
+        }else{
+            return 0;
+        }
 }
