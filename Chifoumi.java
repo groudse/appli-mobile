364d4656fc0b8433d066d21e7e16a80240d01482
Your Name <you@example.com>
2022-07-11 12:04:57 +0200

    nex

---
+++ Chifoumi.java
@@ -0,0 +1,23 @@
+public class Chifoumi {
+    public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
+       
+
+        switch(chifoumiAction){
+
+            case ROCK:
+                return chifoumiAction.SCISSOR;
+                
+
+            case PAPER:
+                return chifoumiAction.ROCK;
+                
+                
+            default:
+                return chifoumiAction.PAPER;
+                
+            
+        }
+        
+       
+    }
+}
--- IsEven.java
+++
@@ -1,9 +0,0 @@
-public class IsEven {
-    public static boolean isEven(int a) {
-        if(a%2==0){
-            return true;
-        }else{
-            return false;
-        }
-    }
-}
