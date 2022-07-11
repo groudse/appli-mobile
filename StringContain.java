a41896204ce7fab9a5b3ad0c4c43788594c8b314
Your Name <you@example.com>
2022-07-11 14:25:31 +0200

    tre

--- StringContain.java
+++ StringContain.java
@@ -1,6 +1,6 @@
 public class StringContain {
     public static boolean isStringContainedIn(String subString, String s) {
-        if(s == null){
+        if(s == null || subString == null){
             return false;
         }else if(s.indexOf(subString) != -1){
             return true;
