ebb374674761a58848492d5c676c865970f54831
Your Name <you@example.com>
2022-07-11 14:16:54 +0200

    tre

---
+++ StringContain.java
@@ -0,0 +1,9 @@
+public class StringContain {
+    public static boolean isStringContainedIn(String subString, String s) {
+        if(s.indexOf(subString) != -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
