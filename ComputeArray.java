4dbbda501db953253ab4513707b66b4ca4dee198
Your Name <you@example.com>
2022-07-11 15:56:37 +0200

    tre

--- ComputeArray.java
+++ ComputeArray.java
@@ -1,12 +1,12 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
-        int stock;
+      
         if(array == null){
         return null;
         }
       for(int i = 0;i<array.length;i++){
          
-        
+    
         if(array[i]%3==0){
             array[i] = array[i]*5;
         }else if((array[i]%3)-1 == 0)
