2d675e77ec86615b7f2e204cd6042a7985a12d49
Your Name <you@example.com>
2022-07-11 14:23:35 +0200

    tre

--- StringContain.java
+++ StringContain.java
@@ -2,7 +2,7 @@ public class StringContain {
     public static boolean isStringContainedIn(String subString, String s) {
         if(s == null){
             return false;
-        }else if(s.toLowerCase().indexOf(subString.toLowerCase()) != -1){
+        }else if(s.toLowerCase().contains(subString.toLowerCase()) != -1){
             return true;
         }else{
             return false;
