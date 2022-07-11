64c4b322e69933d8a407566c43751a01a7b3a0e8
Your Name <you@example.com>
2022-07-11 14:16:11 +0200

    tre

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,6 +1,7 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
-        System.out.println(StringConcat.concat("", "Hello World !"));
+        System.out.println(StringContain.isStringContainedIn("Hell", "Highway to Hell"));
+        System.out.println(StringContain.isStringContainedIn("Hell", "Hello World !"));
+        System.out.println(StringContain.isStringContainedIn("Bonjour", "hello World !"));
     }
 }
