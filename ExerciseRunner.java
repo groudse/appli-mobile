9c180174554e29fd97a21c6e2b767ca91678b256
Your Name <you@example.com>
2022-07-11 14:47:07 +0200

    tre

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,7 +1,6 @@
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(StringContain.isStringContainedIn("Hell", "Highway to Hell"));
-        System.out.println(StringContain.isStringContainedIn("Hell", "Hello World !"));
-        System.out.println(StringContain.isStringContainedIn("Bonjour", "hello World !"));
+        System.out.println(Palindrome.isPalindrome("ressasser"));
+        System.out.println(Palindrome.isPalindrome("Bonjour"));
     }
 }
