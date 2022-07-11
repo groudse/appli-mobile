69f0f45988c052743f1be57773f2c64e3fb8b46d
Your Name <you@example.com>
2022-07-11 13:29:33 +0200

    tre

--- StringLength.java
+++ StringLength.java
@@ -1,6 +1,6 @@
 public class StringLength {
     public static int getStringLength(String s) {
-        if(s != ""){
+        if(s != " "){
         return s.length();
         }else{
             return 0;
