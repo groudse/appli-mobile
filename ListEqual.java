b70f6a58307338d358841a22443139b7bb4bdebb
Your Name <you@example.com>
2022-07-12 11:05:26 +0200

    try

--- ListEqual.java
+++ ListEqual.java
@@ -4,7 +4,7 @@ public class ListEqual {
     public static boolean areListEquals(List<String> list1, List<String> list2) {
        
        if(list1 == null || list2 == null){
-        return null;
+        return false;
        }else{
         if(list1.equals(list2)){
         return true;
