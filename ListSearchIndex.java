e97e70825266882ba15949165049589c73aac231
Your Name <you@example.com>
2022-07-12 09:18:21 +0200

    add list

---
+++ ListSearchIndex.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class ListSearchIndex {
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+       String f;
+       if(list.contains(value)){
+       return list.lastIndexOf(value);
+       
+       }else{
+        return null;
+       }
+    }
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        
+        if(list.contains(value)){
+       return list.indexOf(value);
+       }else{
+        return null;
+       }
+    }
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+       
+        if(list.contains(value)){
+       
+        List myList = new ArrayList();
+        
+
+       for(int i=0;i<list.size();i++){
+            if(list.get(i) == value){
+            myList.add(i);
+        }
+       }
+        return myList;
+       }else{
+        return null;
+       }
+    }
+    
+    
+}
