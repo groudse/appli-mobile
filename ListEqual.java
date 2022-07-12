8883ee4e8d8b3a588d7b6ba249654d0838b37a62
Your Name <you@example.com>
2022-07-12 11:02:52 +0200

    tt

---
+++ ListEqual.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class ListEqual {
+    public static boolean areListEquals(List<String> list1, List<String> list2) {
+       
+        System.out.print(list1.equals(list2));
+        
+
+        return true;
+
+    }
+}
