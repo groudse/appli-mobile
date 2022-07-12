8add170f4cfbf83a19c2aea601254015c352bcbb
Your Name <you@example.com>
2022-07-12 11:14:05 +0200

    try

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,8 +1,7 @@
-import java.util.List;
-
 public class ExerciseRunner {
     public static void main(String[] args) {
-        System.out.println(ListEqual.areListEquals(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Charly", "Emily")));
-        System.out.println(ListEqual.areListEquals(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Emily", "Charly")));
+        System.out.println(ParseDate.parseIsoFormat("2022-04-25T20:51:28.709039322"));
+        System.out.println(ParseDate.parseFullTextFormat("lundi 25 avril 2022"));
+        System.out.println(ParseDate.parseTimeFormat("09 heures du soir, 07 minutes et 23 secondes"));
     }
 }
