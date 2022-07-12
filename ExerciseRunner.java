a1411c7b74922abaccf85c3611d18cb0392a9fe3
Your Name <you@example.com>
2022-07-12 09:22:09 +0200

    add list

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,9 +1,9 @@
 import java.util.List;
 
 public class ExerciseRunner {
-
     public static void main(String[] args) {
-        System.out.println(ListContains.containsValue(List.of(9, 13, 8, 23, 1, 0, 89), 8));
-        System.out.println(ListContains.containsValue(List.of(9, 13, 8, 23, 1, 0, 89), 10));
+        System.out.println(ListSearchIndex.findLastIndex(List.of(9, 13, 89, 8, 23, 1, 0, 89), 89));
+        System.out.println(ListSearchIndex.findFirstIndex(List.of(9, 13, 89, 8, 23, 1, 0, 89), 89));
+        System.out.println(ListSearchIndex.findAllIndexes(List.of(9, 13, 89, 8, 23, 1, 0, 89), 89).toString());
     }
 }
