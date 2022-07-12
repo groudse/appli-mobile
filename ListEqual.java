5ae11cd6faf6ecf5da1172c31a31e970c823d804
Your Name <you@example.com>
2022-07-12 11:07:42 +0200

    try

--- ListEqual.java
+++ ListEqual.java
@@ -3,10 +3,10 @@ import java.util.List;
 public class ListEqual {
     public static boolean areListEquals(List<String> list1, List<String> list2) {
        
-       if(list1 == null || list2 == null){
-        return false;
-       }else if(list1 == null && list2 == null){
+       if(list1 == null && list2 == null){
         return true;
+       }else if(list1 == null || list2 == null){
+        return false;
         }else if(list1.equals(list2)){
         return true;
 
