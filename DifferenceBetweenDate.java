f0551414bed73bb3958c519866e2352ff0e7d66d
Your Name <you@example.com>
2022-07-13 15:06:17 +0200

    ef

--- DifferenceBetweenDate.java
+++ DifferenceBetweenDate.java
@@ -8,22 +8,35 @@ public class DifferenceBetweenDate {
 
     public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
        
-    
+        if(localTime1 != null && localTime2 != null){
        Duration dd = Duration.between(localTime1,localTime2);
        return dd;
+    }else{
+        return null;
     }
+}
 
     public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        
+        if(date1 != null && date2 != null){
         Period difference = Period.between(date1, date2);
         return difference;
+
+         } else{
+            return null;
+        }
     }
 
-
     public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        
+        if(dateTime1 != null && dateTime2 != null){
         Duration difference = Duration.between(dateTime1, dateTime2);
         
         long p = difference.toHours();
         return p;
+    } else{
+        return null;
     }
+}
 
 }
