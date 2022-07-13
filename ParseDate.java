19c65b335799af14763fef2faa9dc6597a894de8
Your Name <you@example.com>
2022-07-13 09:54:22 +0200

    last

--- ParseDate.java
+++ ParseDate.java
@@ -9,31 +9,38 @@ import java.util.Locale;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if(!stringDate == null){
         LocalDateTime parseIt = LocalDateTime.parse(stringDate);
         return parseIt;
+    }else{
+        return null;
     }
 
   public static LocalDate parseFullTextFormat(String stringDate) {
-
+    if(!stringDate == null){
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy", Locale.FRANCE);
     LocalDate dt = LocalDate.from(formatter.parse(stringDate));
     return dt;
 
+    }else{
+        return null;
     }
 
  
     public static LocalTime parseTimeFormat(String stringDate) {
         
         
-    //09 heures du soir, 07 minutes et 23 secondes
-    //H +"heures"+ B, m +"minutes et"+ s +"secondes" "
     
+    if(!stringDate == null){
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh 'heures' B, mm 'minutes et' ss 'secondes'", Locale.FRANCE);
     LocalTime dt = LocalTime.from(formatter.parse(stringDate));
     return dt;
 
 
 
+    }else{
+        return null;
+    }
     }
 
 }
