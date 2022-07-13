8e8ea877f427ae9023b45c14648968cec746b687
Your Name <you@example.com>
2022-07-13 11:49:48 +0200

    tcfjfluegzig

--- FormatDate.java
+++ FormatDate.java
@@ -29,7 +29,7 @@ public class FormatDate {
         if(pp==19){
        String dateString = dateTime.toString();
        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-       DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("'Le' dd MMM. 'de l''an' yyyy 'à' HH'h'mm'm et' ss's'");
+       DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("'Le' dd MMM. 'de l''an' yyyy 'à' HH'h'mm'm et' ss's'", Locale.FRANCE);
        LocalDateTime dateT = LocalDateTime.parse(dateString, formatter1);
        String sortie = formatter2.format(dateT);
        return sortie;
