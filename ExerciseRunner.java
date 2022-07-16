e2c8380a531cac8ecde8be9d2b1f7f2139bb7d4c
Your Name <you@example.com>
2022-07-16 17:27:44 +0200

    tte

--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -4,6 +4,6 @@ public class ExerciseRunner {
 
     public static void main(String[] args) {
         System.out.println(Sort.sort(List.of(15, 1, 14, 18, 14, 98, 54, -1, 12)).toString());
-        System.out.println(Sort.sortReverse(List.of(15, 1, 14, 18, 14, 98, 54, -1, 12)).toString());
+        //System.out.println(Sort.sortReverse(List.of(15, 1, 14, 18, 14, 98, 54, -1, 12)).toString());
     }
 }
--- Sort.java
+++ Sort.java
@@ -1,40 +1,46 @@
 import java.util.*;
+import java.util.ArrayList;
 
+import javax.swing.plaf.synth.SynthEditorPaneUI;
 
 public class Sort {
 
     public static List<Integer> sort(List<Integer> list) {
-        List<Integer> myList = new ArrayList<>();
-        
+    
+        ArrayList<Integer> list2 = new ArrayList<Integer>();   
+        list2.clear();
         if(list != null){
+        for(int i=0; i<list.size();i++ ){
+            list2.add(list.get(i));
 
-            Collections.sort(list);
-            for(int i=0; i<list.size();i++ )
-        {
-            myList.add(list.get(i));
         }
-        return myList;
-        }else{
-            myList.clear();
-            return myList;
-        }
+        
+        Collections.sort(list2);   
+    
+        return list2;
+    }else{
+        return list2;
+    }
+ 
+     
+        
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
-        List<Integer> myList = new ArrayList<>();
-       
+        ArrayList<Integer> list2 = new ArrayList<Integer>();   
+        list2.clear();
         if(list != null){
-            Collections.sort(list, Collections.reverseOrder());
-            for(int i=0; i<list.size();i++ )
-        {
-            myList.add(list.get(i));
-        }
-        return myList; 
-        }else{
-            myList.clear();
+        for(int i=0; i<list.size();i++ ){
+            list2.add(list.get(i));
 
-            return myList;
         }
+        
+        Collections.sort(list2,Collections.reverseOrder());   
+    
+        return list2;
+    }else{
+        return list2;
+    }
     }
 }
 
