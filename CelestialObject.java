f2d6ccd0dd1e26c727f77e74f822ee3a347a8c61
Your Name <you@example.com>
2022-07-18 11:37:21 +0200

    fyf

--- CelestialObject.java
+++ CelestialObject.java
@@ -4,14 +4,14 @@ public class CelestialObject {
     public double z;
     public String name;
     
-    CelestialObject(){
+    public CelestialObject(){
         x = 0.0;
         y = 0.0;
         z = 0.0;
         name = "Soleil";
     }
 
-    CelestialObject(String f,double v1,double v2,double v3){
+    public CelestialObject(String f,double v1,double v2,double v3){
         x = v1;
         y = v2;
         z = v3;
