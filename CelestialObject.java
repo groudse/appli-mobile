07cf5b6b037e08ec60192d9b5a787d70dac9095b
Your Name <you@example.com>
2022-07-18 15:50:13 +0200

    ee

--- CelestialObject.java
+++ CelestialObject.java
@@ -1,9 +1,13 @@
 public class CelestialObject {
+   
     public double x;
     public double y;
     public double z;
     public String name;
-    
+    final static double KM_IN_ONE_AU = 150000000;
+
+
+
     public CelestialObject(){
         x = 0.0;
         y = 0.0;
@@ -49,6 +53,23 @@ public class CelestialObject {
     public String getName(){
         return name;
     } 
+
+    public static double getDistanceBetween(CelestialObject obj1,CelestialObject obj2){
+        double d = Math.pow((Math.pow(obj2.getX() - obj1.getX(), 2) +
+                         Math.pow(obj2.getY() - obj1.getY() , 2) +
+                         Math.pow(obj2.getZ()  - obj1.getZ() , 2) *
+                                    1.0), 0.5);
+        
+    
+        return d;
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject obj1,CelestialObject obj2){
+        
+        double distanceKm = CelestialObject.getDistanceBetween(obj1, obj2)* KM_IN_ONE_AU;
+        
+        return distanceKm;
+    }
 }
 
 
