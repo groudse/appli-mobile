3a3a485336fa5bf5808c6e35a44bf92acaefa33d
Your Name <you@example.com>
2022-07-18 11:05:18 +0200

    hehudfgduieg

--- CelestialObject.java
+++ CelestialObject.java
@@ -1,16 +1,20 @@
 public class CelestialObject {
-    public static double x(double sortie){
-        return sortie;
-    }
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+    // public  double x(double sortie){
+    //     return sortie;
+    // }
 
     
-    public static double y(double sortie){
-        return sortie;
-    }
-    public static double z(double sortie){
-        return sortie;
-    }
-    public static String name(String sortie){
-        return sortie;
-    }
+    // public static double y(double sortie){
+    //     return sortie;
+    // }
+    // public static double z(double sortie){
+    //     return sortie;
+    // }
+    // public static String name(String sortie){
+    //     return sortie;
+    // }
 }
