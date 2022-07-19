7bfe7afca202907a582bb6ff01866ea3f7f95ebd
Your Name <you@example.com>
2022-07-19 15:48:54 +0200

    r

--- Star.java
+++ Star.java
@@ -1,6 +1,29 @@
+import java.text.DecimalFormat;
+
 public class Star extends CelestialObject{
 
     private double magnitude;
+
+
+    public Star(){
+        
+    }
+
+    public Star(String name,double x,double y,double z,double magnitude){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.magnitude = magnitude;
+    }
+
+
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("0.000");
+        String fff = this.getName() + " shines at the " + df.format(magnitude) +" mangitude";
+                    
+         return fff;
+    }            
 
     public double getMagnitude(){
         return magnitude;
@@ -9,5 +32,7 @@ public class Star extends CelestialObject{
     public void setMagnitude(double magnitude){
         this.magnitude = magnitude;
     }
+
+
 
 }
