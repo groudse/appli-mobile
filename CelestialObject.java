4ca43b19729c324250ec1d85a6babb853b963f2f
Your Name <you@example.com>
2022-07-19 09:51:14 +0200

    r

--- CelestialObject.java
+++ CelestialObject.java
@@ -3,7 +3,7 @@ public class CelestialObject {
     public double x;
     public double y;
     public double z;
-    public String name;
+    public String name = "e";
     public final static double KM_IN_ONE_AU = 150000000;
 
 
