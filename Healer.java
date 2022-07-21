df20a0da18131b74b77df281fe94f242ad8e8657
Your Name <you@example.com>
2022-07-21 10:56:10 +0200

    hh

---
+++ Healer.java
@@ -0,0 +1,12 @@
+public class Healer {
+    
+    public int healCapacity;
+
+    public void heal(Character perso){
+
+    }
+
+    public int getHealCapacity(){
+        return healCapacity;
+    }
+}
---
+++ Tank.java
@@ -0,0 +1,13 @@
+public class Tank {
+    
+    private int shield;
+
+
+    public Tank(){
+
+    }
+
+    public int getShield(){
+        return shield;
+    }
+}
