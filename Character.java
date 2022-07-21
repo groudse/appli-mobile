afd481b8f62583f51e9c2e23e03d18cde1680ae2
Your Name <you@example.com>
2022-07-21 10:33:54 +0200

    j

--- Character.java
+++ Character.java
@@ -1,17 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
     private final  int maxHealth; 
     private int currentHealth; 
     private final String name; 
+    private static List<Character> allCharacters = new ArrayList<>();
 
 
     public Character(String name, int maxHealth){
         this.name = name;
         this.currentHealth = maxHealth;
         this.maxHealth = maxHealth;
+        allCharacters.add(this);
     }
 
+    public static String printStatus(){
+        
+        List<Character> total = new ArrayList<>(allCharacters);
 
+        if(total.isEmpty() == false){
+        String fin = new String();
+        String line = "------------------------------------------";
 
+        String txt = "\nCharacters currently fighting :\n";
+
+        for(int i =0;i<total.size();i++){
+        String info = " - " + total.get(i).toString() + "\n";
+        fin = fin + info;
+        }
+
+        return line + txt + fin +line;
+    }else{
+        String line = "------------------------------------------";
+        String txt = "\nNobody's fighting right now !\n";
+        return line + txt + line;
+    }
+    }
+
+    public static Character fight(Character f1,Character f2){
+        int i = 1;
+        while(f1.getCurrentHealth() > 0 && f2.getCurrentHealth() > 0){
+            if(i%2==0){
+                f2.attack(f1);
+            }else{
+                f1.attack(f2);
+
+            }
+            i++;
+        }
+
+        if(f1.getCurrentHealth()<0){
+            return f2;
+
+        }else{
+            return f1;
+
+        }
+    }
 
     public int getMaxHealth(){
         return maxHealth;
@@ -29,9 +75,7 @@ public class Character {
     public void takeDamage(int dmg ){
         if(currentHealth>0){
         currentHealth -= dmg;
-        if(currentHealth<0){
-            currentHealth = currentHealth - currentHealth;
-        }}
+        }
     }
 
     public void attack(Character badGuy){
@@ -41,9 +85,8 @@ public class Character {
     public String toString(){
 
         String statusChara;
-   
         if(this.currentHealth > 0){
-            statusChara = getName() + " : " + this.getCurrentHealth()+ "/" + getMaxHealth();
+            statusChara = getName() + " : " + this.getCurrentHealth()+ "/" + this.getMaxHealth();
         }else{
             statusChara = getName() + " : KO"; 
         }
--- ExerciseRunner.java
+++ ExerciseRunner.java
@@ -1,18 +1,16 @@
 public class ExerciseRunner {
 
     public static void main(String[] args) {
+        System.out.println(Character.printStatus());
+
         Character aragorn = new Character("Aragorn", 20);
-        Character uruk = new Character("Uruk", 5);
-        
-        System.out.println(aragorn.toString());
-        System.out.println(uruk.toString());
-        
-        aragorn.attack(uruk);
+        Character uruk = new Character("Uruk", 15);
 
-        System.out.println(uruk.toString());
-        
-        aragorn.takeDamage(12);
+        System.out.println(Character.printStatus());
 
-        System.out.println(aragorn.toString());
+        Character winner = Character.fight(aragorn, uruk);
+
+        System.out.println(winner.toString());
+        System.out.println(Character.printStatus());
     }
 }
