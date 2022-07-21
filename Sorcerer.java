5e83dec18a3e7510e3e02bc39e81059e1b1afe96
Your Name <you@example.com>
2022-07-21 11:27:10 +0200

    tyui

--- Sorcerer.java
+++ Sorcerer.java
@@ -20,10 +20,10 @@ public class Sorcerer extends Character implements Healer {
     public String toString(){
         if(this.getCurrentHealth() > 0){
         return this.getName() +" is a sorcerer with "+ this.getCurrentHealth() +
-        " HP. It can heal "+ this.getHealCapacity() +" HP";
+        " HP. It can heal "+ this.getHealCapacity() +" HP.";
         }else{
             return this.getName()+ " is a dead sorcerer. So bad, it could heal "
-            + this.getHealCapacity() + " HP";
+            + this.getHealCapacity() + " HP.";
         }
     }
 }
