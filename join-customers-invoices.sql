ce89ba64c65a32116d66120ce00ac33718499035
Your Name <you@example.com>
2022-07-24 22:54:57 +0200

    uu

--- join-customers-invoices.sql
+++ join-customers-invoices.sql
@@ -1,1 +1,2 @@
-SELECT FirstName,LastName,Company,Total AS InvoiceTotalPrice FROM customers INNER JOIN invoices on customers.CustomerId = invoices.CustomerId;
+SELECT FirstName,LastName,Company,Total AS InvoiceTotalPrice FROM customers INNER JOIN invoices on customers.CustomerId = invoices.CustomerId
+WHERE Company IS NOT NULL ;
