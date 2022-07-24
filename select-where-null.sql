82fd78b8e36352afb4faf03af65c474d971dfd72
Your Name <you@example.com>
2022-07-24 22:28:42 +0200

    uu

--- select-where-null.sql
+++ select-where-null.sql
@@ -1,1 +1,1 @@
-SELECT * FROM invoices WHERE BillingState  IS NULL AND BillingPostalCode IS NULL AND Total>10;
+SELECT * FROM invoices WHERE BillingState  IS NULL AND BillingPostalCode IS NULL AND Total>10 ;
