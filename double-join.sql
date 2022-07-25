76a2b6ade69944b45f89e53d69402e85a6df4d97
Your Name <you@example.com>
2022-07-25 13:59:40 +0200

    uu

--- double-join.sql
+++ double-join.sql
@@ -1,4 +1,4 @@
 SELECT distinct tracks.Name AS TrackName,pt.Name AS PlaylistName FROM tracks
     INNER JOIN playlist_track p on tracks.TrackId = p.TrackId
     INNER JOIN playlists pt on p.PlaylistId = pt.PlaylistId
-    WHERE pt.Name='TV Shows' LIMIT 100;
+    WHERE pt.Name='TV Shows' LIMIT 100 ;
