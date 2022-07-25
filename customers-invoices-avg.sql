40d1d61de143394d717d800fbea30fb450f9aec0
Your Name <you@example.com>
2022-07-25 14:50:06 +0200

    uu

---
+++ customers-invoices-avg.sql
@@ -0,0 +1,3 @@
+SELECT  FirstName, LastName, AVG(Total) as InvoicesAverage FROM customers
+         INNER JOIN invoices i on customers.CustomerId = i.CustomerId
+         GROUP BY  i.CustomerId  ORDER BY FirstName ASC;
