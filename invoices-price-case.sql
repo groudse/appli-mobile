0656c272f954b617730e11b6cad5b8c57d70b5b0
Your Name <you@example.com>
2022-07-28 23:52:30 +0200

    e

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -1,8 +1,6 @@
-SELECT InvoiceId, Total,
+SELECT InvoiceId,Total,
     CASE
         WHEN Total<5 THEN 'Price lower than 5$'
-        WHEN Total<10 THEN 'Price lower than 10$'
-        WHEN Total>10 THEN 'Price greater than 10$'
-    END
-FROM invoices
-    LIMIT 100;
+        WHEN Total<=10 THEN 'Price lower than 10$'
+        ELSE 'Price greater than 10$'
+    END as 'CASE' FROM invoices LIMIT 100;
