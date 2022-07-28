e386b7e2f4f8817bca29ddf4db92369b791dacc6
Your Name <you@example.com>
2022-07-28 23:22:20 +0200

    e

--- total-invoices.sql
+++ total-invoices.sql
@@ -1,3 +1,3 @@
-SELECT FirstName || ' ' || upper(LastName) AS FullName, round(sum(i.Total),2) AS AllInvoices
+SELECT FirstName || ' ' || upper(LastName) AS FullName, sum(i.Total) AS AllInvoices
 FROM customers INNER JOIN invoices i on customers.CustomerId = i.CustomerId
 group by FullName having AllInvoices>=38.00 ORDER BY FullName ASC;
