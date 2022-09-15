53c2bbddf635c6a1af5e79d435e29e0c4a22ffd5
Your Name <you@example.com>
2022-09-15 15:42:58 +0200

    r

--- customers-invoices-avg.sql
+++ customers-invoices-avg.sql
@@ -1,3 +1,4 @@
 SELECT  FirstName, LastName, AVG(Total) as InvoicesAverage FROM customers
          INNER JOIN invoices i on customers.CustomerId = i.CustomerId
          GROUP BY  i.CustomerId  ORDER BY FirstName ASC;
+
--- genre-tracks-average.sql
+++ genre-tracks-average.sql
@@ -1,2 +1,3 @@
 SELECT genres.NAME, avg(Milliseconds) AS AverageDuration FROM genres INNER JOIN tracks t on genres.GenreId = t.GenreId
 GROUP BY genres.NAME ORDER BY AverageDuration DESC;
+
