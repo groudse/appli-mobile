94345da956114aea2ff21151c23085d0b57d3402
Your Name <you@example.com>
2022-09-16 10:07:27 +0200

    e

---
+++ customers-continent.sql
@@ -0,0 +1,8 @@
+SELECT FirstName,LastName,Country,
+    CASE
+        WHEN Country IN ('Chile','Argentina','USA','Canada','Brazil') THEN 'America'
+        WHEN Country IN ('United Kingdom','Sweden','Spain','Poland','Netherlands','Italy','Ireland','Hungary','Finland','Portugal','Denmark','Belgium','Austria' ,'Norway','France','Germany','Norway','Czech Republic')  THEN  'Europe'
+        WHEN Country IN ('India') THEN 'Asia'
+        WHEN Country IN ('Australia') THEN 'Oceania'
+    END as continent
+FROM customers;
