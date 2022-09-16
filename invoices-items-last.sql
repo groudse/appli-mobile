6b5f1271dabd2b91fa1df023e65a8e209968686a
Your Name <you@example.com>
2022-09-16 15:38:49 +0200

    e

--- invoices-items-last.sql
+++ invoices-items-last.sql
@@ -1,3 +1,3 @@
-SELECT Name,InvoiceId FROM tracks JOIN invoice_items ii on tracks.TrackId = ii.TrackId
+SELECT Name FROM tracks JOIN invoice_items ii on tracks.TrackId = ii.TrackId
 WHERE ii.InvoiceId = (SELECT InvoiceId from invoices WHERE InvoiceDate =
 (SELECT MAX (InvoiceDate) FROM invoices) )
