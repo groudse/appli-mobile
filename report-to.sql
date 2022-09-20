34041c430a3b7c1eedb67ac44c1b231985c61bc6
Your Name <you@example.com>
2022-09-20 15:50:02 +0200

    e

--- report-to.sql
+++ report-to.sql
@@ -1,5 +1,10 @@
-select  y.EmployeeId, (y.FirstName|| " " || y.LastName) as EmployeeName,
-(x.FirstName|| " " || x.LastName) as ReportsTo
-from employees y, employees x
-where y.ReportsTo = x.EmployeeId
+select  v.EmployeeId, (v.FirstName|| " " || v.LastName) as EmployeeName,
+CASE
+    WHEN v.ReportsTo is not null then (x.FirstName|| " " || x.LastName)
+    WHEN v.ReportsTo is null then null
+end as ReportsTo
+from employees v
+left join employees x on v.ReportsTo = x.EmployeeId
+
+
 
