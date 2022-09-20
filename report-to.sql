f17402c3b172806f8488f4b35bae096be3d6cd3c
Your Name <you@example.com>
2022-09-20 16:16:27 +0200

    e

--- report-to.sql
+++ report-to.sql
@@ -1,7 +1,7 @@
 select  v.EmployeeId, (v.FirstName|| " " || v.LastName) as EmployeeName,
 CASE
     WHEN v.ReportsTo is not null then (x.FirstName|| " " || x.LastName)
-    WHEN v.ReportsTo is null then null
+    WHEN v.ReportsTo is null then 'NULL'
 end as ReportsTo
 from employees v
 left join employees x on v.ReportsTo = x.EmployeeId
